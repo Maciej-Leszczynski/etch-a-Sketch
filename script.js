1e378965ef0cf0a1578e8198f2a56c8def79d86d
Maciej <leszczynski.m@interia.pl>
2020-01-09 16:42:34 +0100

    Added control for the reset button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let grid = document.getElementById('grid');
 let br = document.createElement('br');
 
-
+// Add divs to the grid
 for (let i = 0; i <= 15; i++) {
     for (let j = 0; j <= 15; j++) {
         let gridBoxes = document.createElement('div');
@@ -10,9 +10,23 @@ for (let i = 0; i <= 15; i++) {
     }
 }
 
+// Sketching functionality
 let boxes = document.querySelectorAll(".boxes");
 Array.from(boxes).forEach((box) => {
     box.addEventListener('mouseenter', (e) => {
         e.target.style.background = 'black';
+        e.target.style.borderColor = 'white';
     })
 });
+
+// Reset button
+let reset = document.getElementById('reset');
+
+reset.addEventListener('click', clearSketch);
+
+function clearSketch() {
+    Array.from(boxes).forEach((box) => {
+        box.style.background = 'white';
+        box.style.borderColor = 'black';
+    })
+}
