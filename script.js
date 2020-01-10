e94dd6e8803d7153667cb7815c1918fd05079e8f
Maciej <leszczynski.m@interia.pl>
2020-01-10 14:18:33 +0100

    Random colors button functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let br = document.createElement('br');
 let defaultSize = 16;
 let reset = document.getElementById('reset');
 let resize = document.getElementById('size');
+let colors = document.getElementById('colors');
+
 
 // Add divs to the grid
 function drawGrid(size) {
@@ -65,9 +67,45 @@ function changeStyle(percent) {
     });
 }
 
+// colors mode
+function sketchInColors() {
+    let boxes = document.querySelectorAll(".boxes");
+    Array.from(boxes).forEach((box) => {
+        box.addEventListener('mouseenter', (e) => {
+            e.target.style.background = rgbRandomizer();
+            e.target.style.borderColor = rgbRandomizer();
+        })
+    });
+}
+
+function rgbRandomizer() {
+    let min = 1;
+    let max = 255;
+    let r = Math.floor(Math.random() * (max - min) + min)
+    let g = Math.floor(Math.random() * (max - min) + min)
+    let b = Math.floor(Math.random() * (max - min) + min)
+    return `rgb(${r},${g},${b})`
+}
+
+function colorsMode() {
+    let onOffValue = document.getElementById('colors').value;
+    let body = document.querySelector("body");
+    if (onOffValue === 'off') {
+        colors.value = 'on';
+        body.style.backgroundColor = rgbRandomizer();
+        grid.style.backgroundColor = "white";   
+        sketchInColors();
+    } else {
+        colors.value = 'off';
+        body.style.backgroundColor = "white";
+        sketch();
+    }
+}
+
 // MAIN
 drawGrid(defaultSize);
 sketch();
 
 reset.addEventListener('click', clearSketch);
 resize.addEventListener('click', changeSize);
+colors.addEventListener('click', colorsMode);
